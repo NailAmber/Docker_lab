99fd5bd92e4fd7d98cb251a46eb9eab6434ebbc3
nailambe <noemail@example.com>
2025-08-28 19:53:38 +0700

    format code and add .gitignore

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import os
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def index():
-    return jsonify(status="ok", message="Hello from Flask in Docker!", env=os.getenv("APP_ENV", "dev"))
+    return jsonify(
+        status="ok",
+        message="Hello from Flask in Docker!",
+        env=os.getenv("APP_ENV", "dev"),
+    )
+
 
 @app.get("/healthz")
 def healthz():
     return "ok", 200
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
