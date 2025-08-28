122102f8c69047505e482ed1c0912ab0872a0d9e
Nailambe <48957779+NailAmber@users.noreply.github.com>
2025-08-28 18:56:20 +0700

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app as flask_app
+
+@pytest.fixture
+def client():
+    flask_app.config['TESTING'] = True
+    with flask_app.test_client() as client:
+        yield client
+
+def test_index(client):
+    rv = client.get("/")
+    assert rv.status_code == 200
+    json_data = rv.get_json()
+    assert "status" in json_data and json_data["status"] == "ok"
+
+def test_healthz(client):
+    rv = client.get("/healthz")
+    assert rv.status_code == 200
+    assert rv.get_data(as_text=True) == "ok"
